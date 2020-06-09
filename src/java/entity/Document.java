37e32810ddf7613cfc445e7b52eff02d682aa0a2
seyfsalis <61714993+seyfsalis@users.noreply.github.com>
2020-06-09 15:17:48 +0300

    document entity sınıfı oluşturuldu

---
+++ src/java/entity/Document.java
@@ -0,0 +1,42 @@
+package entity;
+
+public class Document {
+
+    private int id;
+    private String filePath;
+    private String fileName;
+    private String fileType;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+}
